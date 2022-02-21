6099bfc262fe62b48bce39fb0d32187185f38e87
Park heejung <spoa12@naver.com>
2022-02-21 14:53:10 +0900

    init project

---
+++ src/main/java/planshare/server/planshare/PlanShareApplication.java
@@ -0,0 +1,13 @@
+package planshare.server.planshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/planshare/server/planshare/PlanShareApplicationTests.java
@@ -0,0 +1,13 @@
+package planshare.server.planshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
