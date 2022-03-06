ad6765b2c54dcae0f129ad1deff2035b5bb4a77d
Johoseong <whghtjd320@naver.com>
2022-03-06 15:45:54 +0900

    feat: JWT토큰 유효성 검사 기능 추가
    - resolveToken에서 request의 헤더에서 JWT토큰 추출.
    - validateToken에서 토큰 유효성 검사.
    - getAuthentication에서 토큰 내 유저 정보 조회 후 userDetails에 담아서 authentication으로 반환.

--- src/main/java/planshare/server/planshare/util/JWTUtil.java
+++ src/main/java/planshare/server/planshare/util/JWTUtil.java
@@ -1,39 +1,84 @@
 package planshare.server.planshare.util;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Date;
 
+
+@RequiredArgsConstructor
 @Component
 public class JWTUtil {
-    public String makeJWT(Long id, String email) {
+
+    @Value("${jwt.secret}")
+    private String key;
+
+    private final UserDetailsService userDetailsService;
+
+    // key Base64로 인코딩
+    @PostConstruct
+    protected void init() {
+        key = Base64.getEncoder().encodeToString(key.getBytes());
+    }
+
+    // jwt token 생성
+    public String createJWT(Long id, String email) {
         Date now = new Date();
 
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
+                .setIssuedAt(now) // 발급시간
+                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis())) // 만료시간
                 .claim("id", id)
                 .claim("email", email)
-                .signWith(SignatureAlgorithm.HS256, "secret")
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
 
-    public Long getJwtId(String JWT) {
-        return Long.parseLong(Jwts.parser()
-                .setSigningKey("secret")
-                .parseClaimsJws(JWT)
-                .getBody().get("id").toString());
+    // JWT 토큰에서 인증(유저) 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getJwtId(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public String getJwtEmail(String JWT) {
+    // 토큰에서 유저정보 parsing
+    public String getJwtId(String token) {
         return (String) Jwts.parser()
-                .setSigningKey("secret")
-                .parseClaimsJws(JWT)
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody().get("id").toString();
+    }
+
+    public String getJwtEmail(String token) {
+        return (String) Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
                 .getBody().get("email");
+    }
+
+    // header에서 token 값 추출
+    public String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        return token.substring("Bearer ".length());
+    }
+
+    // 토큰 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
