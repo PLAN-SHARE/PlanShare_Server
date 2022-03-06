9741150790857e0b5adc50b56d9dce3a1f1d4999
Johoseong <whghtjd320@naver.com>
2022-03-06 16:05:41 +0900

    feat: Oauth 인가code로 accessToken 받는 controller 추가
    - acessToken에 대한 DTO 생성.
    - code로 accessToken response로 받는 controller 추가.

--- src/main/java/planshare/server/planshare/user/controller/UserController.java
+++ src/main/java/planshare/server/planshare/user/controller/UserController.java
@@ -1,7 +1,8 @@
 package planshare.server.planshare.user.controller;
 
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,23 @@ public class UserController {
 
     private final UserService userService;
 
+    // accessToken -> user 정보 받아서 jwt토큰 생성 후 반환
     @GetMapping("/user/login")
     public String login(@RequestParam String token){
         return userService.login(token);
+    }
+
+    // code -> accessToken 반환
+    @GetMapping("/test/code")
+    public String getAccessToken(@RequestParam String code) {
+        String jwtToken = userService.getAccessToken(code);
+        return jwtToken;
+    }
+
+    @GetMapping("/checkJWT")
+    public String list() {
+        //권한체크
+        Authentication user = SecurityContextHolder.getContext().getAuthentication();
+        return (String) user.getPrincipal();
     }
 }
--- src/main/java/planshare/server/planshare/user/service/UserService.java
+++ src/main/java/planshare/server/planshare/user/service/UserService.java
@@ -1,13 +1,17 @@
 package planshare.server.planshare.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import planshare.server.planshare.user.dto.KakaoUserInfo;
+import planshare.server.planshare.user.dto.TokenResponse;
 import planshare.server.planshare.util.JWTUtil;
 
 @RequiredArgsConstructor
@@ -15,6 +19,9 @@ import planshare.server.planshare.util.JWTUtil;
 public class UserService {
 
     private final JWTUtil jwtUtil;
+
+    @Value("${client.id}")
+    private String clientId;
 
     public String login(String token) {
 
@@ -34,6 +41,35 @@ public class UserService {
         );
 
         //jwt토큰 생성
-        return jwtUtil.makeJWT(resp.getBody().getId(), resp.getBody().getKakaoAccount().getEmail());
+        return jwtUtil.createJWT(resp.getBody().getId(), resp.getBody().getKakaoAccount().getEmail());
+    }
+
+    // code를 통해 accessToken 받음
+    public String getAccessToken(String code) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", "application/json");
+        headers.add("User-Agent", "api-test");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", clientId);
+        params.add("redirect_uri", "http://localhost:9090/login");
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+
+        RestTemplate rt = new RestTemplate();
+
+        // {code} 를 이용해 access_token 요청
+        ResponseEntity<TokenResponse> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                entity,
+                TokenResponse.class
+        );
+        headers.clear();
+
+        return response.getBody().getAccess_token();
     }
 }
