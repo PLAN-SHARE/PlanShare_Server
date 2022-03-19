1869a33acd164568ea5495d0277e045eb6419097
Park heejung <spoa12@naver.com>
2022-03-19 18:34:11 +0900

    feat: Goal color, iconNumber, visibility 속성 추가 및 search by memberId 기능 추가(#13)

--- src/main/java/planshare/server/planshare/domain/Goal.java
+++ src/main/java/planshare/server/planshare/domain/Goal.java
@@ -1,5 +1,7 @@
 package planshare.server.planshare.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import javax.persistence.*;
@@ -8,6 +10,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Goal {
 
     @Id
@@ -17,13 +20,24 @@ public class Goal {
 
     private String name;
 
+    // HEX COLOR CODE;
+    private String color;
+
+    private int iconNumber;
+
+    // 공개 여부
+    private boolean visibility;
+
     @ManyToOne
     @JoinColumn(name = "m_id")
     private Member member;
 
-    public static Goal createGoal(String name, Member member){
+    public static Goal createGoal(String name, String color, int iconNumber, boolean visibility, Member member){
         Goal goal = new Goal();
         goal.name = name;
+        goal.color = color;
+        goal.iconNumber = iconNumber;
+        goal.visibility = visibility;
         goal.member = member;
 
         return goal;
@@ -33,4 +47,10 @@ public class Goal {
         this.name = name;
     }
 
+    public void modifyGoal(String name, String color, int iconNumber, boolean visibility){
+        this.name = name;
+        this.color = color;
+        this.iconNumber =iconNumber;
+        this.visibility = visibility;
+    }
 }
